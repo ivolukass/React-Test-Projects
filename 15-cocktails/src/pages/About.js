6f2a014989c7d82228a23d238fada6eea135108b
ivolukass <ivolukass@gmail.com>
2021-03-01 10:29:58 +0200

    init

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident nemo
+        suscipit deleniti? Maiores atque commodi nobis iusto adipisci officiis
+        doloremque velit fuga reiciendis, excepturi ullam expedita animi
+        obcaecati nisi itaque. Reiciendis magnam, cumque repudiandae cum totam
+        similique voluptates impedit aliquam!
+      </p>
+    </section>
+  );
+};
+
+export default About;
